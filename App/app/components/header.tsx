324d9315c6796b1590b64d7060806b92ccfb48fb
ethutami uta <ethutami@gmail.com>
2025-08-22 19:51:29 +0800

    responsive navbar

--- App/app/components/header.tsx
+++ App/app/components/header.tsx
@@ -16,10 +16,10 @@ const Navbar: React.FC = () => {
     ];
 
     return (
-        <header className="w-full shadow-sm px-20">
+        <header className="w-full shadow-sm px-5 md:px-10 lg:px-20 ">
             <div className="flex items-center justify-between">
                 <Logo />
-                <div className="flex-1 mx-6">
+                <div className="hidden md:block flex-1 mx-4">
                     <div className="relative">
                         <input
                             type="search"
@@ -31,7 +31,7 @@ const Navbar: React.FC = () => {
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search-icon lucide-search absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"><path d="m21 21-4.34-4.34" /><circle cx="11" cy="11" r="8" /></svg>
                     </div>
                 </div>
-                <nav className="hidden md:flex space-x-6 text-sm font-medium">
+                <nav className="hidden md:block text-sm font-medium space-x-4 mx-4 ">
                     {navItems.map((item) => (
                         <Link
                             key={item.href}
@@ -45,12 +45,22 @@ const Navbar: React.FC = () => {
                         </Link>
                     ))}
                 </nav>
-                <div className="flex items-center space-x-4 ml-4">
-                    <button className="p-2 hover:bg-gray-100 rounded-full">
+                <div className="hidden md:block flex items-center space-x-4">
+
+                    <button className="hover:bg-gray-100 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bell-icon lucide-bell"><path d="M10.268 21a2 2 0 0 0 3.464 0" /><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" /></svg>
                     </button>
-                    <button className="p-2 hover:bg-gray-100 rounded-full">
+                    <button className="hover:bg-gray-100 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></svg>
+                    </button>
+                </div>
+                <div className="block md:hidden lg:hidden flex items-center space-x-4">
+                    <button className=" hover:bg-gray-100 rounded-full">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34" /><circle cx="11" cy="11" r="8" /></svg>
+                    </button>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu-icon lucide-menu"><path d="M4 12h16" /><path d="M4 18h16" /><path d="M4 6h16" /></svg>
                     </button>
                 </div>
             </div>
--- App/app/components/logo.tsx
+++ App/app/components/logo.tsx
@@ -1,14 +1,12 @@
 const Logo = () => {
     return (
         <div className="flex flex-row items-center">
-
-
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="113.6 104 307 214"
                 width="70"
                 height="48.75"
-                className="block text-[#091057] dark:text-[#FFFFFF]"
+                className="hidden lg:block text-[#091057] dark:text-[#FFFFFF]"
             >
                 <g fill="currentColor">
                     <circle cx="269.2" cy="148.4" r="44" />
@@ -19,9 +17,25 @@ const Logo = () => {
                     <path d="M319.2 248 A50.8 41.6 0 0 1 420.8 248 L419 272 C417.2 284.8 411.6 288 392.4 288 H351.2 C332 288 326.4 284.8 325.2 272 L319.2 248Z" />
                 </g>
             </svg>
-            <div className="ml-6" >
-                <p className="font-bold text-[30px] text-[#091057] dark:text-[#FFFFFF]">Conferency</p>
-                <p className="font-medium text-[16px] text-[#EC8305]">Event Management</p>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="113.6 104 307 214"
+                width="47"
+                height="32.5"
+                className="lg:hidden text-[#091057] dark:text-[#FFFFFF]"
+            >
+                <g fill="currentColor">
+                    <circle cx="269.2" cy="148.4" r="44" />
+                    <circle cx="173.2" cy="152.4" r="34.8" />
+                    <circle cx="367.2" cy="152.4" r="34.8" />
+                    <path d="M194.8 260 A75.2 57.6 0 0 1 344.4 260 L339.6 294.4 C336.4 312.8 324.4 318.4 295.6 318.4 H244.4 C215.6 318.4 203.6 312.8 200.4 294.4 L194.8 260Z" />
+                    <path d="M113.6 248 A50.8 41.6 0 0 1 215.2 248 L212 272 C210 284.8 204.4 288 185.2 288 H143.6 C124.4 288 118.8 284.8 116.8 272 L113.6 248Z" />
+                    <path d="M319.2 248 A50.8 41.6 0 0 1 420.8 248 L419 272 C417.2 284.8 411.6 288 392.4 288 H351.2 C332 288 326.4 284.8 325.2 272 L319.2 248Z" />
+                </g>
+            </svg>
+            <div className="ml-2 md:ml-3 lg:ml-6" >
+                <p className="font-bold text-[24px] lg:text-[26px] md:text-[24px] text-[#091057] dark:text-[#FFFFFF]">Conferency</p>
+                <p className="font-medium text-[14px] lg:text-[16px] text-[#EC8305]">Event Management</p>
             </div>
         </div>
 
@@ -29,3 +43,5 @@ const Logo = () => {
 }
 
 export default Logo
+
+
