4cbdb1069d9a6daf58804133393f8cada4937c63
ethutami uta <ethutami@gmail.com>
2025-09-05 15:13:19 +0800

    change margin titlesection

--- conferency/app/components/titleSection.tsx
+++ conferency/app/components/titleSection.tsx
@@ -2,7 +2,7 @@ export const TitleSection = ({ title }: { title: string }) => {
     return (
         <h2 className="section-title mt-16 mb-6 inline-block">
             {title}
-            <div className="mt-4 w-[60px] md:w-[80px] lg:w-[100px] h-[4px] md:h-[6px] lg:h-[8px] bg-[#091057] dark:bg-[#fff] rounded"></div>
+            <div className="mt-2 w-[60px] md:w-[80px] lg:w-[100px] h-[4px] md:h-[6px] lg:h-[8px] bg-[#091057] dark:bg-[#fff] rounded"></div>
         </h2>
     )
 }
