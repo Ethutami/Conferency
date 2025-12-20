bc52a1068792254a0f19b84634bc7efd71b09489
ethutami uta <ethutami@gmail.com>
2025-12-20 10:39:57 +0800

    update  component

--- conferency/app/components/titleSection.tsx
+++ conferency/app/components/titleSection.tsx
@@ -1,6 +1,6 @@
-export const TitleSection = ({ title }: { title: string }) => {
+export const TitleSection = ({ title, styles }: { title: string, styles?: string }) => {
     return (
-        <h2 className="section-title mt-16 mb-6 inline-block">
+        <h2 className={`section-title mt-16 mb-6 inline-block ${styles}`}>
             {title}
             <div className="mt-2 w-[60px] md:w-[80px] lg:w-[100px] h-[4px] md:h-[6px] lg:h-[8px] bg-[#091057] dark:bg-[#fff] rounded"></div>
         </h2>
