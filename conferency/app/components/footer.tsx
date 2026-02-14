72523af2e4ee1fb105d226952a81ed4816714a1e
ethutami uta <ethutami@gmail.com>
2026-02-14 15:21:30 +0800

    responsive css

--- conferency/app/components/footer.tsx
+++ conferency/app/components/footer.tsx
@@ -6,26 +6,26 @@ import Logo from "./logo"
 export default function Footer() {
     return (
         <div >
-            <div className="flex flex-row justify-between mb-16 px-4 md:px-8 lg:px-16">
+            <div className="flex flex-col items-center md:flex-row md:justify-between mb-16 px-4 md:px-8 lg:px-16 mt-10">
                 <div className="flex flex-col gap-4">
                     <Logo />
-                    <p className="font-medium text-sm leading-normal text-[var(--muted)] italic">
+                    <p className="hidden md:block font-medium text-sm leading-normal text-[var(--muted)] italic">
                         Developed by&nbsp;
                         <span className="font-medium text-[var(--base)]">
                             Ehtutami
                         </span>
                     </p>
                 </div>
-                <div className="flex flex-col gap-4">
-                    <p className="tertiary-title">Contact & Support</p>
-                    <div className="flex flex-row gap-6">
-                        <span className="flex items-center gap-2 text-[var(--base)] hover:text-[var(--secondary)] cursor-pointer transition-colors">
+                <div className="flex flex-col gap-4 mt-4">
+                    <p className="hidden md:block tertiary-title ">Contact & Support</p>
+                    <div className="flex flex-row md:flex-col md:gap-4 lg:flex-row gap-6 ">
+                        <span className="flex items-center gap-2 text-xs md:text-sm text-[var(--base)] hover:text-[var(--secondary)] cursor-pointer transition-colors">
                             <FiMail /><span>Ehtutami@gmail.com</span>
                         </span>
-                        <span className="flex items-center gap-2 text-[var(--base)] hover:text-[var(--secondary)] cursor-pointer transition-colors">
+                        <span className="flex items-center gap-2 text-xs md:text-sm text-[var(--base)] hover:text-[var(--secondary)] cursor-pointer transition-colors">
                             <FiLinkedin /><span>Ehtutami</span>
                         </span>
-                        <span className="flex items-center gap-2 text-[var(--base)] hover:text-[var(--secondary)] cursor-pointer transition-colors">
+                        <span className="flex items-center gap-2 text-xs md:text-sm text-[var(--base)] hover:text-[var(--secondary)] cursor-pointer transition-colors">
                             <FiInstagram /><span>@_Ehtuta</span>
                         </span>
                     </div>
