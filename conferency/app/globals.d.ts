c0bde18ddb8badf479a54fb6ce8c2e9abe233674
ethutami uta <ethutami@gmail.com>
2026-02-14 19:27:15 +0800

    fixing minor bugs

--- conferency/app/globals.d.ts
+++ conferency/app/globals.d.ts
@@ -3,3 +3,4 @@ declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
 }
+declare module "*.css";
--- conferency/app/layout.tsx
+++ conferency/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./tailwind.css";
 import "./globals.scss";
 
 const geistSans = Geist({
